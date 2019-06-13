44fa337e3dbea07927a2cd4b1d9a7b6472853262
unknown <mohdkhalid0807@gmail.com>
2019-06-13 17:22:45 +0530

    initial commit for hello world skill

--- lambda/custom/express-server.js
+++ lambda/custom/express-server.js
@@ -1,70 +1,4 @@
-# Build An Alexa Hello World Skill With Express-With-Lambda
-
-### This is a simple tutorial to introduce a simple Alexa skill with Express-With-Lambda.
-
-## Step 1 : 
-    Clone this [hello world skill](https://github.com/alexa/skill-sample-nodejs-hello-world.git)
-
-### Add express-server.js file
-> Create a express-server.js inside lambda/custom/
-> Add following code in it.
-`'use strict'
-const express = require('express'),
-      bodyParser = require('body-parser'),
-      app = express();
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
-app.post("/", async(req, res) => {
-
-    res.send("Hello World!");
-});
-
-module.exports = app;`
-
-## Step 2 :
-    We're using claudia.js to automate the process of creating AWS lambda function and API gateway.
-
-### Install Claudia and configure AWS credential
-    `npm install -g claudia`
-
-1.Create an AWS profile with IAM full access, Lambda full access and API Gateway Administrator privileges.
-2.Add the keys to your .aws/credentials file
-`[claudia]
-aws_access_key_id = YOUR_ACCESS_KEY
-aws_secret_access_key = YOUR_ACCESS_SECRET`
-
-Note : If we configured aws credentails before then we can skip above point 2.
-
-### Generate AWS Lambda wrapper for Express app
-
-To make your app work correctly with AWS Lambda, you need to generate AWS Lambda wrapper for your Express app. With Claudia, you can do so by running the following command in your terminal.
-
-`claudia generate-serverless-express-proxy --express-module express-server --source lambda/custom/ --proxy-module-name express-lambda --profile <aws-profile-name>`
-
-where express-server is a name of an entry file of your Express app, just without the .js extension.
-
-This step generated a file named express-lambda.js
-
-### Deploy Express App
-
-Note : We should remove index.js file inside lambda/custom/
-
-Now you only need to deploy your Express app (with express-lambda.js file) to AWS Lambda and API Gateway using the claudia create command.
-
-`claudia create --source lambda/custom/ --name express-lambda-service  --handler express-lambda.handler --deploy-proxy-api --region us-east-1 --profile <aws-profile-name>`
-
-The command printed the following response:
-
-`{  "lambda": {    "role": "awesome-serverless-expressjs-app-executor",    "name": "awesome-serverless-expressjs-app",    "region": "eu-central-1"  },  "api": {    "id": "iltfb5bke3",    "url": "https://iltfb5bke3.execute-api.eu-central-1.amazonaws.com/latest"  }}
-`
-
-Now test our express-lambda service is working or not by copying the printed url in your terminal, test it with postman, the API should return 'Hello World!'
-
-## Step 3 :
-> Replace `express-server.js` code with following:
-`'use strict'
+'use strict'
 const express = require('express'),
       bodyParser = require('body-parser'),
       Alexa = require('ask-sdk-core'),
@@ -178,24 +112,8 @@ app.post("/", async(req, res) => {
     .create();
 
     const response = await skillBuilder.invoke(req.body, req.apiGateway.context);
+    console.log(`RESPONSE++++${JSON.stringify(response)}`);
 
     res.send(response);
 })
-module.exports = app;`
-
-> Run update command
-    `claudia update --source lambda/custom/ --profile <aws-profile-name>`
-
-## Step 4 :
-
-    Deploy skill and model using `ASK CLI`
-    `ask deploy -t skill`
-    `ask deploy -t model`
-
-## Step 5 :
-> Add https endpoint generated in Step 2 into the Skill Developer Portal Endpoint->HTTPS and SSL certificate type as 'My development endpoint is a sub-domain of a domain that has a wildcard certificate from a certificate authority'.
-> Save Endpoints
-> Save Model
-> Build Model
-
-To check everything has deployed successfully, open the Developer Portal and check the Hello World Skill.
+module.exports = app;
